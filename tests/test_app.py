2f813cde39f77e4d0f36b1abdedd2cbfd39aecd6
thobis <thobias_nh@hotmail.com>
2025-06-15 00:21:38 -0300

    adicionando CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,65 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from src.fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # arrange
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # act
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'hello World!'}
+    assert response.json() == {'message': 'Hello World!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',  # userSchema
+        json={
+            'username': 'testuser',
+            'password': '<PASSWORD>',
+            'email': 'testEMail@test.com',
+        },
+    )
+    # voltou correto?
+    assert response.status_code == HTTPStatus.CREATED
+    # validar userpublic
+    assert response.json() == {
+        'id': 1,
+        'username': 'testuser',
+        'email': 'testEMail@test.com',
+    }
+
+
+def test_read_user(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'testuser',
+                'email': 'testEMail@test.com',
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'id': 1,
+            'username': 'testuser',
+            'email': 'testEMail@test.com',
+            'password': '123',
+        },
+    )
+
+    assert response.json() == {
+        'id': 1,
+        'username': 'testuser',
+        'email': 'testEMail@test.com',
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
